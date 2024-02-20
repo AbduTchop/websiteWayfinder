43bb76a542d673eab50d64c7e6afe9e7fde8ed5b
AbduTchop <115070603+AbduTchop@users.noreply.github.com>
2024-02-20 14:44:03 +0100

    Add files via upload

--- wayfinder/scripts/script.js
+++ wayfinder/scripts/script.js
@@ -1,22 +1,40 @@
 const setup = () => {
     let menuChoice = document.getElementById("menu").value;
     let pad = ["h1.1", "h2.2","h3.3", "forum"];
+    let index = 0;
 
     const updateMenu = () => {
         menuChoice = document.getElementById("menu").value;
         fotoKader.setAttribute("src","#" + menuChoice)
     }
 
-    const padVolgende = () => {
+    const padStart = () => {
         fotoKader.setAttribute("src","#" + pad[0])
+    }
+
+    const padVolgende = () => {
+        if (index !== pad.length -1) {
+            index++;
+        }
+        fotoKader.setAttribute("src","#" + pad[index])
+    }
+
+    const padVorige = () => {
+        if (index !== 0) {
+            index--;
+        }
+        fotoKader.setAttribute("src","#" + pad[index])
     }
 
     let volgendeButton = document.getElementById("volgendeButton");
     let vorigeButton = document.getElementById("vorigeButton");
+    let startButton = document.getElementById("startButton");
     let fotoKader = document.getElementById("image-360");
     let menuveld = document.getElementById("menu");
     menuveld.addEventListener("change", updateMenu);
-    volgendeButton.addEventListener("change", padVolgende);
+    startButton.addEventListener("click", padStart);
+    volgendeButton.addEventListener("click", padVolgende);
+    vorigeButton.addEventListener("click", padVorige);
 }
 
 window.addEventListener("load", setup);
